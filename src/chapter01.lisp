8e569e3f2677866aadc7033516102bdcbbfa2f5c
Tim Hawes <trhawes@gmail.com>
2024-06-28 21:01:49 -0400

    Added answer from PAIP

--- src/chapter01.lisp
+++ src/chapter01.lisp
@@ -29,3 +29,10 @@
       (loop for i from 1 to power
             for result = base then (* result base)
             finally (return result))))
+
+(defun power (x n)
+  "Power raises x to the nth power. N must be an integer >= 0.
+This executes in log n time, because of the check for even n."
+  (cond (( = n 0) 1)
+        ((evenp n) (expt (power x (/ n 2)) 2))
+        (t (* x (power x ( - n 1))))))
